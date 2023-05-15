02b77c569cdb440645ef48f4d8460b85c5e34c66
Guilherme Marques <gmoraesm12@gmail.com>
2023-05-15 19:07:51 -0300

    changes

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque id impedit enim similique quis facilis aliquid, beatae nam? Soluta eligendi blanditiis quibusdam autem commodi eius nobis provident qui nesciunt omnis?',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
